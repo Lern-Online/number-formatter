e87e4b1dd4a01101c39abdde34f2671dc397be1a
GrischaK <grischa.kosba@gmail.com>
2015-11-12 02:26:01 +0100

    copyright russian edtion added

--- tests/Lang/Lv/SpelloutOrdinalMasculineTest.php
+++ tests/Lang/Lv/SpelloutOrdinalMasculineTest.php
@@ -6,10 +6,6 @@
  * @copyright Arius IT Arkadiusz Ostrycharz
  * @homepage http://arius.pl
  *
- * Russian Version:
- * @copyright Grigorij Kosba 
- * @homepage http://www.lern-online.net
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
